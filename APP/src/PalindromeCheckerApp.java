d48d1be89f96de23522357667622028799c45c2e
surya-jagan3 <sg6577@srmist.edu.in>
2026-02-25 15:33:24 +0530

    UC3: string reversal and equals method

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,16 +1,22 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-        public static void main(String[] args) {
-            String input = "madam";
-            String reversed = "";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String reversed = "";
 
-            for (int i = input.length() - 1; i >= 0; i--) {
-                reversed += input.charAt(i);
-            }
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
 
-            boolean isPalindrome = input.equals(reversed);
+        if (input.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
 
-            System.out.println("Input text: " + input);
-            System.out.println("Is it a Palindrome?: " + isPalindrome);
+        scanner.close();
 
     }
 }
