5fabd9542e7097e374f91405e8639bd80bfbd06a
surya-jagan3 <sg6577@srmist.edu.in>
2026-02-25 15:48:12 +0530

    UC4

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String input = "madam";
-        boolean isPalindrome = true;
+        String input = "radar"; [cite: 34]
+        char[] chars = input.toCharArray(); [cite: 35, 36]
 
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+        int start = 0; [cite: 37]
+        int end = chars.length - 1; [cite: 39]
+        boolean isPalindrome = true; [cite: 41]
+
+        while (start < end) { [cite: 42]
+            if (chars[start] != chars[end]) { [cite: 8, 21]
                 isPalindrome = false;
                 break;
             }
+            start++;
+            end--;
         }
 
-        System.out.println("Input text: " + input);
-        System.out.println("Is it a Palindrome?: " + isPalindrome);
+        System.out.println("Input: " + input); [cite: 44]
+        System.out.println("Is Palindrome?: " + isPalindrome); [cite: 45]
 
     }
 }
