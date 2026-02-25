cc8712432010d31daa2aedeee5ec7dd774ffb9a0
surya-jagan3 <sg6577@srmist.edu.in>
2026-02-25 15:56:30 +0530

    UC5

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String input = "radar"; [cite: 34]
-        char[] chars = input.toCharArray(); [cite: 35, 36]
+        public static void main(String[] args) {
+            String input = "noon";
+            Stack<Character> stack = new Stack<>();
 
-        int start = 0; [cite: 37]
-        int end = chars.length - 1; [cite: 39]
-        boolean isPalindrome = true; [cite: 41]
-
-        while (start < end) { [cite: 42]
-            if (chars[start] != chars[end]) { [cite: 8, 21]
-                isPalindrome = false;
-                break;
+            for (char c : input.toCharArray()) {
+                stack.push(c);
             }
-            start++;
-            end--;
-        }
 
-        System.out.println("Input: " + input); [cite: 44]
-        System.out.println("Is Palindrome?: " + isPalindrome); [cite: 45]
+            boolean isPalindrome = true;
+            for (char c : input.toCharArray()) {
+                if (c != stack.pop()) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            System.out.println("Input: " + input);
+            System.out.println("Is Palindrome?: " + isPalindrome);
 
     }
 }
