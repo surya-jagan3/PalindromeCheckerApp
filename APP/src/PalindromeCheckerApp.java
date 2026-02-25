f8e381d1f64e50da868ced9d8f45f443578a292a
Manasviii20 <manasvijhade0209@gmail.com>
2026-02-25 14:31:05 +0530

    UC1-WELCOMEPAGE

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker App");
+
+    }
 }
