37500a14c2ac89d265ac421fef11553fb82f8c42
surya-jagan3 <sg6577@srmist.edu.in>
2026-02-25 15:14:57 +0530

    UC2 : USING FOR LOOP

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,7 +1,17 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker App system");
-        System.out.println("VErsion 1.0");
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome?: " + isPalindrome);
 
     }
 }
